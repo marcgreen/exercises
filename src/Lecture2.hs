862834eed428b655048a01b92853c135fbd3a799
Marc <marcgreen@wpi.edu>
2022-01-18 00:02:45 +0100

    first four problems of lecture 2.

--- src/Lecture2.hs
+++ src/Lecture2.hs
@@ -48,7 +48,11 @@ zero, you can stop calculating product and return 0 immediately.
 84
 -}
 lazyProduct :: [Int] -> Int
-lazyProduct = error "TODO"
+lazyProduct [] = 1
+lazyProduct (x : xs) = case x of
+  0 -> 0
+  _ -> x * lazyProduct(xs) 
+  
 
 {- | Implement a function that duplicates every element in the list.
 
@@ -58,7 +62,8 @@ lazyProduct = error "TODO"
 "ccaabb"
 -}
 duplicate :: [a] -> [a]
-duplicate = error "TODO"
+duplicate [] = []
+duplicate (x:xs) = [x,x] ++ duplicate(xs)
 
 {- | Implement function that takes index and a list and removes the
 element at the given position. Additionally, this function should also
@@ -70,7 +75,13 @@ return the removed element.
 >>> removeAt 10 [1 .. 5]
 (Nothing,[1,2,3,4,5])
 -}
-removeAt = error "TODO"
+removeAt :: Int -> [a] -> (Maybe a, [a])
+removeAt _ [] = (Nothing, [])
+removeAt index (x:xs)
+  | index == 0 = (Just x, xs)
+  | otherwise  = let (e , t) = removeAt (index - 1) xs
+    in (e, x:t)
+-- oh, xs !! index
 
 {- | Write a function that takes a list of lists and returns only
 lists of even lengths.
@@ -81,7 +92,8 @@ lists of even lengths.
 ♫ NOTE: Use eta-reduction and function composition (the dot (.) operator)
   in this function.
 -}
-evenLists = error "TODO"
+evenLists :: [[a]] -> [[a]]
+evenLists = filter (even . length)
 
 {- | The @dropSpaces@ function takes a string containing a single word
 or number surrounded by spaces and removes all leading and trailing
@@ -97,6 +109,7 @@ spaces.
 
 🕯 HINT: look into Data.Char and Prelude modules for functions you may use.
 -}
+dropSpaces :: String -> String
 dropSpaces = error "TODO"
 
 {- |
