6424d48f888e826a0555f03c9e667e1a47306742
Marc <marcgreen@wpi.edu>
2022-01-28 01:36:59 +0100

    impld Foldable

--- test/Test/Lecture3.hs
+++ test/Test/Lecture3.hs
@@ -77,23 +77,18 @@ lecture3Spec = describe "Lecture 3" $ do
         it "Checks duplicates only for original values" $
             appendDiff3 [1] [2] [1, 2] `shouldBe` [1, 2, 1, 2]
 
-{-
-
-!!! UNCOMMENT THE FOLLOWING SECTION FOR FOLDABLE/FUNCTOR TESTS !!!
-
     describe "Laws: Foldable" $ do
         it "List1" $ do
             lawsCheck (foldableLaws genList1With) `shouldReturn` True
         it "Treasure" $ do
             lawsCheck (foldableLaws genTreasureWith) `shouldReturn` True
-
+{-
     describe "Laws: Functor" $ do
         it "List1" $ do
             lawsCheck (functorLaws genList1With) `shouldReturn` True
         it "Treasure" $ do
             lawsCheck (functorLaws genTreasureWith) `shouldReturn` True
 -}
-
 genSmallInt :: Gen Int
 genSmallInt = Gen.int (Range.linear 0 10)
 
